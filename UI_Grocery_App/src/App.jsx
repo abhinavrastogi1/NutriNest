3355a24e69dec7092abf9ecd7514c26aeedc233b
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-08-29 00:38:35 +0530

    solved error

--- UI_Grocery_App/src/App.jsx
+++ UI_Grocery_App/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 import './App.css'
 import Header from './component/Header/Header'
@@ -11,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    < >
+    <>
     <div className="">
      < Header />
      {/* <Outlook /> */}
