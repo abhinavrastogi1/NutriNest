75b84ee955be874cb3ed666c56cf082be4bd66ee
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-10-25 00:29:23 +0530

    Fix file name and import statements

--- UI_Grocery_App/src/main.jsx
+++ UI_Grocery_App/src/main.jsx
@@ -8,7 +8,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./component/Home/Home.jsx";
 import Basket from "./component/Basket/Basket.jsx";
 import ProductsBycategory from "./component/CategoryProducts/ProductsByMainCategory.jsx";
-import ProductsBySubCategory from "./component/CategoryProducts/ProductsBySubCategory.jsx"
+import ProductsBySubCategory from './component/CategoryProducts/ProductsBySubCategory.jsx';  // Should match the file name exactly
 import ProductsBySubSubCategory from "./component/CategoryProducts/ProductsBySubSubCategory.jsx"
 import Search from "./component/Search/Search.jsx";
 import Product from "./component/Product/Product.jsx";
