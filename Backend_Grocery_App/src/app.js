ed2701dae4717bd912bd065748ca0d875acfb8b4
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-10-27 03:55:03 +0530

    added aditional cors configuration

--- Backend_Grocery_App/src/app.js
+++ Backend_Grocery_App/src/app.js
@@ -11,6 +11,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json({ limit: "16kb" }));
 app.use(
   express.urlencoded({
