c33a3dab0e26d06db7041a60517d30a381ce7c11
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-11-07 01:41:15 +0530

    solving cokkkie error

--- Backend_Grocery_App/src/app.js
+++ Backend_Grocery_App/src/app.js
@@ -2,23 +2,25 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorhandler } from "./Middlewares/error.middleware.js";
+
+// Importing routes
+import userRouter from "./Routes/user.route.js";
+import productRouter from "./Routes/product.routes.js";
+import { findProduct } from "./Routes/findProduct.js";
+
 const app = express();
 
+// Configure CORS
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
+// Middleware for parsing requests
 app.use(express.json({ limit: "16kb" }));
 app.use(
   express.urlencoded({
@@ -28,15 +30,13 @@ app.use(
 );
 app.use(express.static("public"));
 app.use(cookieParser());
-//routes import
-import userRouter from "./Routes/user.route.js";
+
+// Routes
 app.use("/api/users", userRouter);
-
-import productRouter from "./Routes/product.routes.js";
-
 app.use("/api/product", productRouter);
-//error hadler midleware
-import { findProduct } from "./Routes/findProduct.js";
 app.use("/api/findProduct", findProduct);
+
+// Error handler middleware
 app.use(errorhandler);
+
 export default app;
