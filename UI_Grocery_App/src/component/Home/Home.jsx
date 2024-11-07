1dbb7aab75b844ecf0da48a4ce217dd03869822a
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2024-11-08 02:03:50 +0530

    changed the margin in home page

--- UI_Grocery_App/src/component/Home/Home.jsx
+++ UI_Grocery_App/src/component/Home/Home.jsx
@@ -7,7 +7,7 @@ import About from "./HeroSectionComponent/About";
 function Home() {
   return (
     <>
-      <main className="mx-48 lg:mx-36 md:mx-24 sm:mx-16 my-8 sideMargin">
+      <main className="mx-4 sm:mx-8 md:mx-16 lg:mx-48 my-8 ">
         <AutoSlider />
         <HeroSection />
         <Bottom_autoSlider />
